3a0af843ac8101938a72bcbde5bbcdec6ab03420
Crowdbotics <team@crowdbotics.com>
2021-07-30 19:58:29 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/testdocker210730_de_22836/MainActivity.java
@@ -0,0 +1,15 @@
+package com.testdocker210730_de_22836;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "testdocker210730_de_22836";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "testdocker210730_de_22836Identifier",
+  url: "https://testdocker210730_de_22836Identifier.botics.co",
+  api: "https://testdocker210730_de_22836Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
